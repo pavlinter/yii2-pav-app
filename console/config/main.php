30cf6836d30b67e42765fbadcbdd7f76773a0f70
Pavel <pavlinter@gmail.com>
2014-11-12 22:20:24 +0200

    .

--- console/config/main.php
+++ console/config/main.php
@@ -3,20 +3,16 @@
 if (YII_ENV_DEV) {
     $params = array_merge(
         require(__DIR__ . '/../../common/config/params.php'),
-        require(__DIR__ . '/params.php')
-    );
-} else {
-    $params = array_merge(
-        require(__DIR__ . '/../../common/config/params.php'),
         require(__DIR__ . '/../../common/config/params-local.php'),
         require(__DIR__ . '/params.php'),
         require(__DIR__ . '/params-local.php')
     );
+} else {
+    $params = array_merge(
+        require(__DIR__ . '/../../common/config/params.php'),
+        require(__DIR__ . '/params.php')
+    );
 }
-
-
-
-
 
 return [
     'id' => 'app-console',
--- environments/prod&dev/backend/config/main-local.php
+++ environments/prod&dev/backend/config/main-local.php
@@ -4,14 +4,14 @@ return [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
-            // unique CSRF cookie parameter for backend (set by kartik-v/yii2-app-practical-a)
+            // unique CSRF cookie parameter for backend
             'csrfParam' => '_backendCsrf',
         ],
-        // unique identity cookie parameter for backend (set by kartik-v/yii2-app-practical-a)
+        // unique identity cookie parameter for backend
         'user' => [
             'identityCookie' => [
                 'name' => '_backendUser', // unique for backend
-                'path' => '/practical-a/backend' // set it to correct path for backend app.
+                'path' => '/backend' // set it to correct path for backend app.
             ]
         ]
     ],
