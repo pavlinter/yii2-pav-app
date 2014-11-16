a0e20041803a9aec993b29da19ce480ed1210d91
Pavel <pavlinter@gmail.com>
2014-11-16 16:10:53 +0200

    .

--- environments/prod&dev/common/config/main.php
+++ environments/prod&dev/common/config/main.php
@@ -2,6 +2,9 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
